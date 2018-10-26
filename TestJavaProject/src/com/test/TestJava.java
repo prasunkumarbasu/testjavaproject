57afb94cc1cb523dfbf40e8deabd1368ec6f144f
prasunkumarbasu <44499014+prasunkumarbasu@users.noreply.github.com>
2018-10-26 13:40:08 -0400

    Update TestJava.java

--- TestJavaProject/src/com/test/TestJava.java
+++ TestJavaProject/src/com/test/TestJava.java
@@ -4,7 +4,7 @@ public class TestJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test Java Project 1");
+		System.out.println("Test Java Project 123");
 	}
 
 }
